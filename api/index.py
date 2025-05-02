61f41061b3c6e45ae506bdfa4723a8ca0258c02d
danjobanjo03 <bajenovdan@gmail.com>
2025-05-01 23:23:19 -0400

    init of sqlalchemy

--- api/index.py
+++ api/index.py
@@ -4,6 +4,12 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from projects.job_tracker.app import app as tracker_app
+from projects.job_tracker.models import db
+
+try:
+    db.init_app(tracker_app)
+except RuntimeError:
+    pass
 
 root_app = Flask("root_app")
 app = DispatcherMiddleware(root_app, {
