3c5a3e08ac7e43e612c3ab9fdde06c50138187d4
danjobanjo03 <bajenovdan@gmail.com>
2025-05-01 23:21:53 -0400

    remove duplicate call

--- api/index.py
+++ api/index.py
@@ -5,11 +5,6 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from projects.job_tracker.app import app as tracker_app
 
-# ——————— ADD THESE TWO LINES ———————
-from projects.job_tracker.models import db
-db.init_app(tracker_app)
-# ————————————————————————————————
-
 root_app = Flask("root_app")
 app = DispatcherMiddleware(root_app, {
     '/api': tracker_app
