690ca4fb1390dffbc37beef91dd8910c7782b0b6
danjobanjo03 <bajenovdan@gmail.com>
2025-05-01 23:25:53 -0400

    Changes to make vercel work better

--- api/index.py
+++ api/index.py
@@ -1,17 +1,8 @@
 import sys, os
 from flask import Flask
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from projects.job_tracker.app import app as tracker_app
-from projects.job_tracker.models import db
 
-try:
-    db.init_app(tracker_app)
-except RuntimeError:
-    pass
-
-root_app = Flask("root_app")
-app = DispatcherMiddleware(root_app, {
-    '/api': tracker_app
-})
+# Expose the Flask app directly for Vercel
+app = tracker_app
